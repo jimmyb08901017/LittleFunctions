89b7614d2aa176ad8222a11fa358b75af3a42dd1
jimmyb08901017 <jimmy312495@gmail.com>
2024-01-19 18:02:14 +0800

    docs: add comments

--- PythonDecorators/template.py
+++ PythonDecorators/template.py
@@ -1,8 +1,16 @@
+# This is a template of using decorator.
+# Author: Hsiang-Chun Yang
 from functools import wraps
 
-
+# A decorator has function passed in as argument, and
+# a wrapper that wraps the function in some way you 
+# want it to be. For the `@decorator` to get the
+# wrapper, you have to return the wrapper.
 def decorator(func):
-    @wraps(func) # this copys the info of func
+    # @wraps() is a decorator itself!
+    # It copies the informations in function, so
+    # you can use them in wrapper().
+    @wraps(func) 
     def wrapper(*args, **kwargs):
         print("before func:", func.__name__)
         ret = func(*args, **kwargs)
